dbe3a7ef2fe3dd3652ea5e1ec62e9805e838811d
RijalAdil <rijal23diy@gmail.com>
2021-11-15 12:52:55 +0700

    export datatable

--- application/views/pages/teknik/datatable.php
+++ application/views/pages/teknik/datatable.php
@@ -305,16 +305,6 @@
 		</div>
 </div>
 
+
+
  
-<script src="<?php echo base_url(); ?>assets/js/jquery-3.5.1.js"></scrip>
-  <script src="<?php echo base_url(); ?>assets/js/jquery.dataTables.min.js"></scrip>
-  <script src="<?php echo base_url(); ?>assets/js/dataTables.buttons.min.js"></scrip>
-  <script src="<?php echo base_url(); ?>assets/js/jszip.min.js"></scrip>
-  <script src="<?php echo base_url(); ?>assets/js/pdfmake.min.js"></scrip>
-  <script src="<?php echo base_url(); ?>assets/js/vfs_fonts.js"></scrip>
-  <script src="<?php echo base_url(); ?>assets/js/buttons.html5.min.js"></scrip>
-  <script src="<?php echo base_url(); ?>assets/js/buttons.print.min.js"></scrip>
- 
-  
-  <link href="<?php echo base_url(); ?>assets/css/buttons.dataTables.min.css" rel="stylesheet">
-  <link href="<?php echo base_url(); ?>assets/css/jquery.dataTables.min.css" rel="stylesheet">   
--- application/views/template/footer/index.php
+++ application/views/template/footer/index.php
@@ -140,8 +140,18 @@
 
 
 
-
-
+<script src="<?php echo base_url(); ?>assets/js/jquery-3.5.1.js"></script>
+  <script src="<?php echo base_url(); ?>assets/js/jquery.dataTables.min.js"></script>
+  <script src="<?php echo base_url(); ?>assets/js/dataTables.buttons.min.js"></script>
+  <script src="<?php echo base_url(); ?>assets/js/jszip.min.js"></script>
+  <script src="<?php echo base_url(); ?>assets/js/pdfmake.min.js"></script>
+  <script src="<?php echo base_url(); ?>assets/js/vfs_fonts.js"></script>
+  <script src="<?php echo base_url(); ?>assets/js/buttons.html5.min.js"></script>
+  <script src="<?php echo base_url(); ?>assets/js/buttons.print.min.js"></script>
+ 
+  
+  <link href="<?php echo base_url(); ?>assets/css/buttons.dataTables.min.css" rel="stylesheet">
+  <link href="<?php echo base_url(); ?>assets/css/jquery.dataTables.min.css" rel="stylesheet">   
 
 </body>
 
