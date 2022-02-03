e163f880d0b4a1b10bc0b2197f212c6e832183fb
RijalAdil <rijal23diy@gmail.com>
2022-02-03 17:57:03 +0700

    asdasdsad

--- application/views/pages/marketing/client_process/datatable.php
+++ application/views/pages/marketing/client_process/datatable.php
@@ -1,3 +1,4 @@
+<script src="//ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
 <div class="container-fluid">
        <div class="card shadow mb-4">
             <div class="card-header py-3">
@@ -36,7 +37,7 @@
 								<td><a  target="_blank" href="<?php echo base_url(). 'upload/'.$c->upload ; ?>" class="btn-primary view-pdf" ><?php echo $c->no_po ?></a></td>
 								<td><?php echo $c->name ?></td>
 								<td><?php
-								$string = $c->info_po;
+									$string = $c->info_po;
 									if (strlen($string) > 500) {
 
 									    // truncate string
@@ -51,36 +52,12 @@
 
 								?></td>
 								<td><?php if ( (in_array($this->session->userdata('level'), array(0,1,2))) ) { ?>	
-									<b>Bid Price :</b> <span style="color: red;"><?php echo number_format($c->price_bid ,0,',','.') ?></span><br>
-									<b>Price     :</b> <span style="color: green;"><?php echo number_format($c->price ,0,',','.') ?></span>
+									<b>Bid Price :</b> <span style="color: red;"><?php echo number_format($c->price_penawaran ,0,',','.') ?></span><br>
+									<b>Price     :</b> <span style="color: green;"><?php echo number_format($c->price_po ,0,',','.') ?></span>
 									<?php }?>
 								</td>
 								<td>
-									<?php if ( $c->date_1  === NULL or $c->date_1 === '0000-00-00'): ?>
-									        
-									<?php else: ?>
-									        <b>Contact :</b> <?php echo $c->date_1 ?> , <?php echo $c->process_1 ?><hr>
-									<?php endif; ?>
-									<?php if ( $c->date_2  === NULL or $c->date_2 === '0000-00-00'): ?>
-									        
-									<?php else: ?>
-									        <b>Surat Penawaran :</b> <?php echo $c->date_2 ?> , <?php echo $c->process_2 ?><hr>
-									<?php endif; ?>
-									<?php if ( $c->date_3  === NULL or $c->date_3 === '0000-00-00'): ?>
-									        
-									<?php else: ?>
-									        <b>Konfirmasi :</b> <?php echo $c->date_3 ?> , <?php echo $c->process_3 ?><hr>
-									<?php endif; ?>
-									<?php if ( $c->date_4  === NULL or $c->date_4 === '0000-00-00'): ?>
-									        
-									<?php else: ?>
-									        <b>Negosiasi Harga :</b> <?php echo $c->date_4 ?> , <?php echo $c->process_4 ?><hr>
-									<?php endif; ?>
-									<?php if ( $c->date_5  === NULL or $c->date_5 === '0000-00-00'): ?>
-									        
-									<?php else: ?>
-									        <b>PO / Kontrak :</b> <?php echo $c->date_5 ?> , <?php echo $c->process_5 ?><hr>
-									<?php endif; ?>
+									 Kosong
 								</td>
 								<td><?php echo $c->product_name ?></td>
 								<td  class="row justify-content-center">	
@@ -153,84 +130,28 @@
 							</tr>
 							<tr>
 								<td>
-									<div class="form-group">
-										<label for="inputdefault">Description:</label>
-										<textarea class="form-control" type='text' rows="" id="description" name="description"></textarea>
-									</div>
+							        <div class="form-group">
+							            <div id="inputFormRow_pic">
+							                <div class="input-group mb-3">
+							                    <input size="100" type="text" id="pic" name="pic[]" class="form-control m-input" placeholder="NO Penwaran" autocomplete="off">
+							                </div>
+							                 <div class="input-group mb-3">
+							                    <input type="date" id="date" name="date[]" class="form-control m-input" placeholder="date" autocomplete="off">
+							                </div>
+							                    <input type="number" id="price" name="price[]" class="form-control m-input" placeholder="price" autocomplete="off">
+							                    <textarea class="form-control" type='text' rows="" id="address2" name="address2"></textarea>
+							                    <div class="input-group-append">                
+							                        <button id="removeRow_pic" type="button" class="btn btn-danger">-</button>
+							                    </div>
+							                
+							                </div>
+							            </div>
+							            <div id="newRow_pic"></div>
+							            <button id="addRow_pic" type="button" class="btn btn-facebook">+Add</button>
+							        </div>
 								</td>
 							</tr>
-							<tr>
-								<td>
-									<div class="form-group ">
-										<label for="inputdefault">Status Project:</label>
-										<select class="form-control" name="product_id">
-					
-											<option value="">Pilih</option>
-											<?php 
-											foreach($products as $p){ 
-											?>
-											<option value="<?php echo $p->id ?>"><?php echo $p->name ?></option>
-											<?php }?>
-										</select>
-									</div>
-								</td>
-							</tr>
-							<tr>
-								<td>
-									<div class="form-group">
-										<label for="inputdefault">Bid Price:</label>
-										<input type="number" class="form-control" id="price_bid" name="price_bid" required><br>
-										<label for="inputdefault">Price:</label>
-										<input type="number" class="form-control" id="price" name="price" required><br>
-									</div>
-								</td>
-							</tr>
-							<tr>
-								<td colspan="2" >
-									<div class="form-group">
-									<label for="inputdefault">Contact:</label>
-									<input type="date" class="form-control" id="date_1" name="date_1" required><br>
-									<textarea class="form-control" type='text' rows="" id="process_1" name="process_1" placeholder="Information Contact"></textarea>
-									</div>
-								</td>
-							</tr>
-							<tr>
-								<td colspan="2" >
-									<div class="form-group">
-									<label for="inputdefault">Surat Penawaran:</label>
-									<input type="date" class="form-control" id="date_2" name="date_2"><br>
-									<textarea class="form-control" type='text' rows="" id="process_2" name="process_2" placeholder="Information Surat Penawaran"></textarea>
-									</div>
-								</td>
-							</tr>
-							<tr>
-								<td colspan="2" >
-									<div class="form-group">
-									<label for="inputdefault">Konfirmasi:</label>
-									<input type="date" class="form-control" id="date_3" name="date_3" ><br>
-									<textarea class="form-control" type='text' rows="" id="process_3" name="process_3" placeholder="Information Konfirmasi"></textarea>
-									</div>
-								</td>
-							</tr>
-							<tr>
-								<td colspan="2" >
-									<div class="form-group">
-									<label for="inputdefault">Negosiasi:</label>
-									<input type="date" class="form-control" id="date_4" name="date_4"><br>
-									<textarea class="form-control" type='text' rows="" id="process_4" name="process_4" placeholder="Information Negosiasi"></textarea>
-									</div>
-								</td>
-							</tr>
-							<tr>
-								<td colspan="2" >
-									<div class="form-group">
-									<label for="inputdefault">PO / Kontrak:</label>
-									<input type="text"  placeholder="NO PO" class="form-control" id="no_po" name="no_po" ><br>
-									<input type="date" class="form-control" id="date_5" name="date_5" ><br>
-									<textarea class="form-control" type='text' rows="" id="process_5" name="process_5" placeholder="Information PO / Kontrak"></textarea><br>									
-									</div>
-								</td>
-							</tr>
+							
 							
 						</table>
 					</div>
@@ -314,111 +235,7 @@
 										</select>
 									</div>
 								</td>
-							<tr>
-								<td>
-									<div class="form-group">
-										<label for="inputdefault">Description:</label>
-										<textarea class="form-control" type='text' rows="" id="description" name="description"><?php echo $c->description ?></textarea>
-									</div>
-								</td>
-							</tr>
-							<tr>
-								<td>
-									<div class="form-group ">
-										<label for="inputdefault">Status Project:</label>
-										<select class="form-control" name="product_id">
-											<?php 
-											foreach($products as $p){ 
-												 if($p->id == $c->product_id) {
-											?>
-											<option value="<?php echo $p->id ?>"><?php echo $p->name ?></option>
-											<?php } }?>
-											
-											<option value="">Pilih</option>
-											<?php 
-											foreach($products as $p){ 
-											?>
-											<option value="<?php echo $p->id ?>"><?php echo $p->name ?></option>
-											<?php }?>
-										</select>
-									</div>
-								</td>
-							</tr>
-							<tr>
-								<td>
-									<div class="form-group">
-										<label for="inputdefault">Bid Price:</label>
-										<input type="number" class="form-control" id="price_bid" name="price_bid" value="<?php echo $c->price_bid ?>"><br>
-										<label for="inputdefault">Price:</label>
-										<input type="number" class="form-control" id="price" name="price" value="<?php echo $c->price ?>"><br>
-									</div>
-								</td>
-							</tr>
-							<tr>
-								<td colspan="2" >
-									<div class="form-group">
-									<label for="inputdefault">Contact:</label>
-									<input type="date" class="form-control" id="date_1" name="date_1" required value="<?php echo $c->date_1 ?>"><br>
-									<textarea class="form-control" type='text' rows="" id="process_1" name="process_1" placeholder="Information Contact"><?php echo $c->process_1 ?></textarea>
-									</div>
-								</td>
-							</tr>
-							<tr>
-								<td colspan="2" >
-									<div class="form-group">
-									<label for="inputdefault">Surat Penawaran:</label>
-									<input type="date" class="form-control" id="date_2" name="date_2" value="<?php echo $c->date_2 ?>"><br>
-									<textarea class="form-control" type='text' rows="" id="process_2" name="process_2" placeholder="Information Surat Penawaran"><?php echo $c->process_2 ?></textarea>
-									</div>
-								</td>
-							</tr>
-							<tr>
-								<td colspan="2" >
-									<div class="form-group">
-									<label for="inputdefault">Konfirmasi:</label>
-									<input type="date" class="form-control" id="date_3" name="date_3" value="<?php echo $c->date_3 ?>"><br>
-									<textarea class="form-control" type='text' rows="" id="process_3" name="process_3" placeholder="Information Konfirmasi"><?php echo $c->process_3 ?></textarea>
-									</div>
-								</td>
-							</tr>
-							<tr>
-								<td colspan="2" >
-									<div class="form-group">
-									<label for="inputdefault">Negosiasi:</label>
-									<input type="date" class="form-control" id="date_4" name="date_4" value="<?php echo $c->date_4 ?>"><br>
-									<textarea class="form-control" type='text' rows="" id="process_4" name="process_4" placeholder="Information Negosiasi"><?php echo $c->process_4 ?></textarea>
-									</div>
-								</td>
-							</tr>
-							<tr>
-								<td colspan="2" >
-									<div class="form-group">
-									<label for="inputdefault">PO / Kontrak:</label>									
-									<input type="text" placeholder="NO PO" class="form-control" id="no_po" name="no_po" value="<?php echo $c->no_po ?>"><br>
-									<input type="date" class="form-control" id="date_5" name="date_5" value="<?php echo $c->date_5 ?>"><br>
-									<textarea class="form-control" type='text' rows="" id="process_5" name="process_5" placeholder="Information PO / Kontrak"><?php echo $c->process_5 ?></textarea><br>				
-									</div>
-								</td>
-							</tr>
-							<!-- <tr>
-								<td>
-									<div class="form-group">
-										<label for="inputdefault">Tipe Process:</label>
-										<select class="form-control" name="status_client">
-											<option>
-												<?php if ($c->status_client === '0') : ?> Contact
-												<?php elseif ($c->status_client === '1'): ?> Client Process
-												<?php else: ?> Client Closing
-												<?php endif; ?>
-											</option><hr>	
-											<option value="0">Contact</option>
-											<option value="1">Client Process</option>
-											<option value="2">Client Closing</option>
-										</select>
-									</div>
-								</td>
-							</tr> -->
-							
+
 						</table>			
 					
 						<div class="modal-footer">
@@ -435,3 +252,36 @@
 		</div>
 </div>
 
+<script type="text/javascript">
+
+    // add row pic
+    $("#addRow_pic").click(function () {
+        var html = '';
+        html += '<div id="inputFormRow_pic">';
+        html += '<div class="input-group mb-3">';
+        html += '<input type="text" name="pic[]" class="form-control m-input" placeholder="Pic Name" autocomplete="off">';
+        html += '<input type="text" name="pic_contact[]" class="form-control m-input" placeholder="Contact Pic" autocomplete="off">';
+        html += '<input type="text" name="pic_email[]" class="form-control m-input" placeholder="pic@example.com" autocomplete="off">';
+        html += '<div class="input-group-append">';
+        html += '<button id="removeRow_pic" type="button" class="btn btn-danger">-</button>';
+        html += '</div>';
+        html += '</div>';
+
+        $('#newRow_pic').append(html);
+    });
+    
+
+// remove row pic
+    $(document).on('click', '#removeRow_pic', function () {
+        $(this).closest('#inputFormRow_pic').remove();
+    });
+
+<?php foreach($penawaran as $p){ ?>
+ 	// remove row pic
+    $(document).on('click', '#removeRow_pic_<?php echo $p->id ?>', function () {
+        $(this).closest('#inputFormRow_pic_<?php echo $p->id ?>').remove();
+        console.log('#removeRow_pic_<?php echo $p->id ?>');
+    });
+<?php }?>
+
+</script>
